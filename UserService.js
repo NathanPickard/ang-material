d84499777b21503131cad097120e3417b85d340d
Nathan Pickard <nathan.pickard87@gmail.com>
2017-04-24 20:28:37 -0700

    Adding UI components and layout API

---
+++ UserService.js
@@ -0,0 +1,28 @@
+'use strict';
+
+angular.module('users').service('userService', UserService);
+
+function UserService($q) {
+  var users = [
+    {
+      name: 'John Waldo',
+      avatar: '',
+      content: 'Yuccie meh coloring book twee. Blog squid trust fund, copper mug kitsch cold-pressed readymade YOLO health goth biodiesel gluten-free lomo austin live-edge pabst.'
+    },
+     {
+      name: 'Chris Dunn',
+      avatar: '',
+      content: 'Semiotics trust fund cardigan chillwave la croix, keffiyeh kinfolk umami pop-up church-key iPhone +1.'
+    },
+     {
+      name: 'Chelsea Snow',
+      avatar: '',
+      content: 'Cronut fingerstache activated charcoal lumbersexual, bitters typewriter hell of organic health goth humblebrag kogi snackwave.'
+    },
+     {
+      name: 'Peter Robbins',
+      avatar: '',
+      content: 'Coloring book pitchfork mixtape, pinterest snackwave pork belly scenester swag glossier edison bulb.'
+    }
+  ]
+}
